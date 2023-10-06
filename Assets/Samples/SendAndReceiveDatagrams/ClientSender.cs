a4219c81b51e86c1c9ba26f11706cddf57463db2
EvgeniySkvortsov <e.skvortsov@chillgaming.net>
2023-10-06 23:57:33 +0300

    example random byte values

--- Assets/Samples/SendAndReceiveDatagrams/ClientSender.cs
+++ Assets/Samples/SendAndReceiveDatagrams/ClientSender.cs
@@ -1,6 +1,7 @@
 using System;
 using Samples.Datagrams;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Samples.SendAndReceiveDatagrams
 {
@@ -12,9 +13,9 @@ namespace Samples.SendAndReceiveDatagrams
             {
                 var testByteDatagram = new TestByteDatagram
                 {
-                    Value1 = 24,
-                    Value2 = 251,
-                    Value3 = 8,
+                    Value1 = (byte) Random.Range(0,255),
+                    Value2 = (byte) Random.Range(0,255),
+                    Value3 = (byte) Random.Range(0,255),
                 };
                 ClientManager.Instance.Client.Send(ref testByteDatagram);
             }
