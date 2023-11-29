048d9d9c3085fe82038b53b516ccd3a83c92a388
EvgeniySkvortsov <e.skvortsov@chillgaming.net>
2023-11-29 22:21:49 +0300

    correct destination_folder for sync_changes

--- Assets/sync_changes.sh
+++ Assets/sync_changes.sh
@@ -3,7 +3,7 @@
 source_folder="/Users/evgeniyskvortsov/UnityProjects/NetFrame/Packages/com.jeskvo.net-frame/NetFrame"
 
 # destination path
-destination_folder="/Users/evgeniyskvortsov/UnityProjects/NetFrame.Net/NetFrame.Net/NetFrame"
+destination_folder="/Users/evgeniyskvortsov/UnityProjects/NetFrame.Net/NetFrame/NetFrame"
 
 # copy
 /usr/bin/rsync -avz --exclude='.git*' --exclude='*.meta' --exclude="*.asmdef" --delete "$source_folder/" "$destination_folder/"
