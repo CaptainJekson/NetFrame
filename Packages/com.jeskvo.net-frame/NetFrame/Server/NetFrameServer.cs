1e6d5d1a1ecb5f553db9571607fe646a48e1a3ba
EvgeniySkvortsov <e.skvortsov@chillgaming.net>
2024-06-26 23:56:07 +0300

    add file exists

--- Packages/com.jeskvo.net-frame/NetFrame/Server/NetFrameServer.cs
+++ Packages/com.jeskvo.net-frame/NetFrame/Server/NetFrameServer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -72,9 +73,10 @@ namespace NetFrame.Server
             {
                 return false;
             }
-
+            
             _isConnectionProtection =
-                !string.IsNullOrWhiteSpace(rsaKeyFullPath) && !string.IsNullOrWhiteSpace(securityToken);
+                !string.IsNullOrWhiteSpace(rsaKeyFullPath) && !string.IsNullOrWhiteSpace(securityToken) 
+                                                           && File.Exists(rsaKeyFullPath);
 
             if (_isConnectionProtection)
             {
